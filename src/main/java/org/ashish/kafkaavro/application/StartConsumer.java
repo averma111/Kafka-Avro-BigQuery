d21bd318c0ca912cf9582c69be581fb786caec7b
Ashish Verma <ashishn8751@gmail.com>
2022-07-26 17:57:03 -0700

    Refactored Producer and Consumer applications

---
+++ src/main/java/org/ashish/kafkaavro/application/StartConsumer.java
@@ -0,0 +1,41 @@
+package org.ashish.kafkaavro.application;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.ashish.kafkaavro.Interface.IKafkaConstants;
+import org.ashish.kafkaavro.consume.ConsumeRecords;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StartConsumer {
+
+    public static  final Logger LOGGER = LoggerFactory.getLogger(StartConsumer.class);
+    public static void main(String[] args) {
+        runConsumer();
+
+    }
+    public static void runConsumer() {
+        Consumer<String, String> consumer = ConsumeRecords.consumeRecords();
+        int noMessageToFetch = 0;
+        while (true) {
+            final ConsumerRecords<String, String> consumerRecords = consumer.poll(1000);
+            if (consumerRecords.count() == 0) {
+                noMessageToFetch++;
+                if (noMessageToFetch > IKafkaConstants.MAX_NO_MESSAGE_FOUND_COUNT)
+                    break;
+                else
+                    continue;
+            }
+            consumerRecords.forEach(record -> {
+                LOGGER.info("Record Key " + record.key());
+                LOGGER.info("Record value " + record.value());
+                //System.out.println("Record partition " + record.partition());
+                //System.out.println("Record offset " + record.offset());
+            });
+            consumer.commitAsync();
+        }
+        consumer.close();
+    }
+
+}
+
---
+++ src/main/java/org/ashish/kafkaavro/application/StartProducer.java
@@ -0,0 +1,37 @@
+package org.ashish.kafkaavro.application;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.ashish.kafkaavro.Interface.IKafkaConstants;
+import org.ashish.kafkaavro.produce.ProduceRecords;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+import java.util.concurrent.ExecutionException;
+
+public class StartProducer {
+
+    public static  final Logger LOGGER = LoggerFactory.getLogger(StartProducer.class);
+    public static void main(String[] args) {
+       runProducer();
+    }
+    public static void runProducer() {
+        Producer<String, String> producer = ProduceRecords.produceRecords();
+        for (int index = 0; index < IKafkaConstants.MESSAGE_COUNT; index++) {
+            final ProducerRecord<String, String> record = new ProducerRecord<String, String>(IKafkaConstants.TOPIC_NAME
+                    ,UUID.randomUUID().toString(),String.valueOf(index));
+            try {
+                RecordMetadata metadata = producer.send(record).get();
+                LOGGER.info("Record sent with key " + index + " to partition " + metadata.partition()
+                        + " with offset " + metadata.offset());
+            } catch (ExecutionException | InterruptedException e) {
+                LOGGER.error("Error in sending record");
+                LOGGER.error(String.valueOf(e));
+            }
+        }
+    }
+}
+
